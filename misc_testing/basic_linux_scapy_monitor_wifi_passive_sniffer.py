1f7ce1dbb8aaab3ab6810044684163bf6bead5c3
Roadbobek <Roadbobek1234@gmail.com>
2025-12-31 17:42:53 +1100

    workinnnnn

--- misc_testing/basic_linux_scapy_monitor_wifi_passive_sniffer.py
+++ misc_testing/basic_linux_scapy_monitor_wifi_passive_sniffer.py
@@ -84,7 +84,7 @@ def packet_handler(pkt):
     #          p = p.payload
 
     # PRINT FORMATTED OUTPUT
-    print(f"CH:{str(channel):>3} | {rssi:>4}dBm | {bssid} | {security:<15} {has_wps:<5} | {band}/{freq:<8} | {display_ssid}")
+    print(f"CH:{channel:>3} | {rssi:>4}dBm | {bssid} | {security:<15} {has_wps:<5} | {band}/{freq}{"MHz":<8} | {display_ssid}")
 
 def start_scanner(iface):
     # 2. CHECK FOR MONITOR MODE
@@ -104,7 +104,7 @@ def start_scanner(iface):
 
     print(f"\n[*] Sniffing on {iface}... Press Ctrl+C to stop.")
     print("-" * 100)
-    print(f"{'CH':<3} | {'SIG':<7} | {'BSSID':<17} | {'SECURITY':<15} {'WPS':<5} | {'BAND/FREQ':<8} | {'SSID'}")
+    print(f"{'CH':<6} | {'SIG':<7} | {'BSSID':<17} | {'SECURITY':<13} {'WPS':<7} | {'BAND / FREQ':<15} | {'SSID'}")
     print("-" * 100)
 
     sniff(iface=iface, prn=packet_handler, store=0)
